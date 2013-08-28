3d3284e23674cff1c2e595b72b74888d55ae0ce3
Maximilian Ludvigsson <maxiludvigsson@gmail.com>
2013-08-28 13:21:14 +0200

    Automated commit at 2013-08-28 11:21:14 UTC by middleman-deploy 0.1.2

---
+++ javascripts/all.js
@@ -0,0 +1,20 @@
+$(function() {  
+  runTest();
+});
+
+runTest = function() {
+  $info = $('.js-info-box').html('Press first cell');
+
+  $('.table').one('click','td', function(event) {
+    var element1 = $(this);
+    $info.html('Clicked on ' + $(this).text() + ', now press content element');
+
+    $('.table').one('click', 'td', function() {
+      var element2 = $(this);
+      var xpath = $.xpath(element1, element2);
+      $info.html('Clicked on ' + element1.text() + ' and ' + element2.text() +
+                  ', xpath result: <br> ' + xpath
+                );
+    });
+  });
+};
